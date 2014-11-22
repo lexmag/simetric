251c736dcbe03771b378b475c27dd72c253028ef
Aleksei Magusev <lexmag@me.com>
2014-11-22 21:55:29 +0100

    Publish to the Hex

--- mix.exs
+++ mix.exs
@@ -4,10 +4,23 @@ defmodule Simetric.Mixfile do
   def project do
     [app: :simetric,
      version: "0.0.1",
-     elixir: "~> 1.0"]
+     elixir: "~> 1.0",
+     description: description,
+     package: package]
   end
 
   def application do
     [applications: []]
+  end
+
+  defp description do
+    "The library provides facilities to perform approximate string matching" <>
+    " and measurement of string similarity/distance."
+  end
+
+  defp package do
+    [contributors: ["Aleksei Magusev"],
+     licenses: ["ISC"],
+     links: %{"GitHub" => "https://github.com/lexmag/simetric"}]
   end
 end
