a241961a69585d9cc6bd0372a4fe8785a3e2a5e7
Aleksei Magusev <lexmag@me.com>
2015-03-02 23:39:52 +0100

    Add docs to `Simetric.Jaro.Winkler`

--- lib/simetric/jaro/winkler.ex
+++ lib/simetric/jaro/winkler.ex
@@ -1,4 +1,24 @@
 defmodule Simetric.Jaro.Winkler do
+  @moduledoc """
+  An implementation of the [Jaro–Winkler](http://en.wikipedia.org/wiki/Jaro–Winkler_distance) distance metric.
+
+  It is designed and best-suited for short strings such as person names.
+  """
+
+  @doc """
+  Returns a float value between 0 (equates to no similarity) and 1 (is an exact match)
+  representing a distance between `str1` and `str2`.
+
+  ## Examples
+
+      iex> Simetric.Jaro.Winkler.compare("dwayne", "duane")
+      0.8400000000000001
+      iex> Simetric.Jaro.Winkler.compare("even", "odd")
+      0.0
+
+  """
+  @spec compare(String.t, String.t) :: 0..1
+
   def compare(str1, str2) do
     case Simetric.Jaro.compare(str1, str2) do
       0.0 -> 0.0
