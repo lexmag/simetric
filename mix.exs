09c4d21eed6a875dce8844b994e9951b93ee476f
Aleksei Magusev <lexmag@me.com>
2016-08-21 16:47:00 +0200

    Fix Elixir v1.4-dev warnings

--- mix.exs
+++ mix.exs
@@ -1,24 +1,24 @@
 defmodule Simetric.Mixfile do
   use Mix.Project
 
-  def project do
+  def project() do
     [app: :simetric,
      version: "0.1.0",
      elixir: "~> 1.0",
-     description: description,
-     package: package]
+     description: description(),
+     package: package()]
   end
 
-  def application do
+  def application() do
     [applications: []]
   end
 
-  defp description do
+  defp description() do
     "The library provides facilities to perform approximate string matching" <>
     " and measurement of string similarity/distance."
   end
 
-  defp package do
+  defp package() do
     [contributors: ["Aleksei Magusev"],
      licenses: ["ISC"],
      links: %{"GitHub" => "https://github.com/lexmag/simetric"}]
