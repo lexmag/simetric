9c957a906ff420182eb68c58d1f422eb9a4d46c0
Aleksei Magusev <lexmag@me.com>
2017-05-20 23:54:15 +0200

    Release v0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Simetric.Mixfile do
 
   def project() do
     [app: :simetric,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.3",
      description: description(),
      package: package()]
