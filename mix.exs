27357719e6067977e8c6d585a25bf0a565e56ff2
Kian-Meng Ang <kianmeng@cpan.org>
2021-04-26 23:10:00 +0800

    Misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the main reference doc for this
    Elixir library which leverage on latest features of ExDoc.

--- mix.exs
+++ mix.exs
@@ -1,26 +1,47 @@
 defmodule Simetric.Mixfile do
   use Mix.Project
 
+  @source_url "https://github.com/lexmag/simetric"
+  @version "0.2.0"
+
   def project() do
-    [app: :simetric,
-     version: "0.2.0",
-     elixir: "~> 1.3",
-     description: description(),
-     package: package()]
+    [
+      app: :simetric,
+      version: @version,
+      elixir: "~> 1.3",
+      package: package(),
+      deps: deps(),
+      docs: docs()
+    ]
   end
 
   def application() do
     [applications: []]
   end
 
-  defp description() do
-    "This library provides facilities to perform approximate string matching " <>
-    "and measurement of string similarity/distance."
+  defp package() do
+    [
+      description: "This library provides facilities to perform approximate string matching "
+        <> "and measurement of string similarity/distance.",
+      maintainers: ["Aleksei Magusev"],
+      licenses: ["ISC"],
+      links: %{"GitHub" => @source_url}
+    ]
   end
 
-  defp package() do
-    [maintainers: ["Aleksei Magusev"],
-     licenses: ["ISC"],
-     links: %{"GitHub" => "https://github.com/lexmag/simetric"}]
+  defp deps do
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [{:"LICENSE.md", [title: "License"]}, "README.md"],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "#{@version}",
+      formatters: ["html"]
+    ]
   end
 end
